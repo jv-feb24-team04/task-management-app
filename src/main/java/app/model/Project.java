095e8e7fbb23662ec908f4c23b7e6832eb50b7ac
Valentyn Sharshon <77666321+ValentynSharshon@users.noreply.github.com>
2024-05-30 16:41:43 +0300

    Created project entity (#3)

---
+++ src/main/java/app/model/Project.java
@@ -0,0 +1,41 @@
+package app.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.SoftDelete;
+
+@Entity
+@Table(name = "projects")
+@SoftDelete(columnName = "is_deleted")
+@Getter
+@Setter
+public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private LocalDate startDate;
+
+    @Column(nullable = false)
+    private LocalDate endDate;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private ProjectStatus projectStatus;
+}
