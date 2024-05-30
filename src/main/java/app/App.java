b7c842f02e59f8a20e771cb3f7c0a5b36368bb81
Yevhenii Vlasenko <159646520+VlasenkoYevhenii@users.noreply.github.com>
2024-05-30 13:22:09 +0300

    added init repo files (#1)
    * added init repo files
    
    * added ci.yml
    
    * changed README.md
    
    * changed java version in ci file 17 -> 21
    
    * changed java version in pom file 17 -> 21
    
    * minor fix

---
+++ src/main/java/app/App.java
@@ -0,0 +1,14 @@
+package app;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@RequiredArgsConstructor
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
+
