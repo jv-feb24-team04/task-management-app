2d10ed3d78f56428595d5503206f5b0d6e12ae7f
Bohdan Markatov <bogdanmarkatov@gmail.com>
2024-05-30 16:02:05 +0300

    some fixes

--- src/main/java/com/example/taskmanagementapplication/model/Attachment.java
+++ src/main/java/com/example/taskmanagementapplication/model/Attachment.java
@@ -2,6 +2,7 @@ package com.example.taskmanagementapplication.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -28,7 +29,9 @@ public class Attachment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
+    @EqualsAndHashCode.Exclude
+    @ToString.Exclude
     @JoinColumn(name = "task_id", nullable = false)
     private Task task;
     @Column(nullable = false)
