318058cec58374909e39d079d7f413b1ab90544f
Yevhenii Vlasenko <yevhenii.vlasenko.work@gmail.com>
2024-05-30 16:45:42 +0300

    added label entity

---
+++ src/main/java/app/model/Label.java
@@ -0,0 +1,36 @@
+package app.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import java.util.List;
+import lombok.Data;
+import org.hibernate.annotations.SoftDelete;
+
+@Entity
+@Table(name = "labels")
+@SoftDelete(columnName = "is_deleted")
+@Data
+public class Label {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String color;
+
+    @ManyToMany
+    @JoinTable(name = "tasks_labels",
+            joinColumns = @JoinColumn(name = "label_id"),
+            inverseJoinColumns = @JoinColumn(name = "task_id"))
+    private List<Task> taskIds;
+}
