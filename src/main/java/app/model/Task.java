3e0037a4d7d1053d2b584da485da1aa55f3332b5
makaroshyna <katya23225@gmail.com>
2024-05-30 17:19:21 +0300

    Created task entity.

---
+++ src/main/java/app/model/Task.java
@@ -0,0 +1,52 @@
+package app.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.SoftDelete;
+
+@Entity
+@Getter
+@Setter
+@SoftDelete(columnName = "is_deleted")
+@Table(name = "tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Priority priority;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TaskStatus status;
+
+    @Column(nullable = false)
+    private LocalDate dueDate;
+
+    @ManyToOne
+    @JoinColumn(
+            name = "project_id",
+            referencedColumnName = "id",
+            nullable = false
+    )
+    private Project project;
+}
