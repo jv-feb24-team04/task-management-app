e48d8d5f753a9be6dbb8d2320cb0fa6640a3b943
Eduard Fakhrutdinov <edikf03@gmail.com>
2024-05-30 22:40:30 +0300

    added comment entity

---
+++ src/main/java/app/model/Comment.java
@@ -0,0 +1,41 @@
+package app.model;
+
+import java.time.LocalDateTime;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import org.hibernate.annotations.SoftDelete;
+
+@Entity
+@Table(name = "comments")
+@SoftDelete(columnName = "is_deleted")
+@Data
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "task_id", nullable = false)
+    private Task task;
+
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    private String text;
+
+    private LocalDateTime timeStamp;
+}
