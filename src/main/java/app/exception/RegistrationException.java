35d8040cf7d73ae4e4d425f104c2578ee6ac009a
Valentyn Sharshon <woosay333@gmail.com>
2024-05-31 11:43:34 +0300

    Added RegistrationException class

---
+++ src/main/java/app/exception/RegistrationException.java
@@ -0,0 +1,11 @@
+package app.exception;
+
+public class RegistrationException extends RuntimeException {
+    public RegistrationException(String message) {
+        super(message);
+    }
+
+    public RegistrationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
