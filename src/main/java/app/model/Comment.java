af090bea0b8a0661a444a59cb88b4c8529c251fd
Eduard Fakhrutdinov <119575377+EduarfF@users.noreply.github.com>
2024-05-31 11:56:26 +0300

    Added comment entity. (#8)
    * added comment entity
    
    * fixed comment entity
    
    * fixed 2 comment entity

---
+++ src/main/java/app/model/Comment.java
@@ -0,0 +1,41 @@
+package app.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@Entity
+@Table(name = "comments")
+@Data
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @ManyToOne
+    @JoinColumn(name = "task_id", nullable = false)
+    private Task task;
+
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private String text;
+
+    @Column(nullable = false)
+    private LocalDateTime timeStamp;
+}
