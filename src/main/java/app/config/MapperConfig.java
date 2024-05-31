6d166f7c6dfa4a695dd9a0236f64bed36e0821b0
Valentyn Sharshon <77666321+ValentynSharshon@users.noreply.github.com>
2024-05-31 14:34:20 +0300

    Added global exception handler and custom exceptions (#10)

---
+++ src/main/java/app/config/MapperConfig.java
@@ -0,0 +1,15 @@
+package app.config;
+
+import org.mapstruct.InjectionStrategy;
+import org.mapstruct.NullValueCheckStrategy;
+import org.mapstruct.ReportingPolicy;
+
+@org.mapstruct.MapperConfig(
+        componentModel = "spring",
+        injectionStrategy = InjectionStrategy.CONSTRUCTOR,
+        nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS,
+        implementationPackage = "<PACKAGE_NAME>.impl",
+        unmappedTargetPolicy = ReportingPolicy.ERROR
+)
+public class MapperConfig {
+}
