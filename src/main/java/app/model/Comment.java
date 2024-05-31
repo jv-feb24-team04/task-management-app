2e598ea8cc46b41d1de439bfbf30e21386dae013
Eduard Fakhrutdinov <edikf03@gmail.com>
2024-05-31 11:35:37 +0300

    fixed comment entity

--- src/main/java/app/model/Comment.java
+++ src/main/java/app/model/Comment.java
@@ -1,6 +1,6 @@
 package app.model;
 
-import java.time.LocalDateTime;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -9,14 +9,13 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import java.time.LocalDateTime;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
-import org.hibernate.annotations.SoftDelete;
 
 @Entity
 @Table(name = "comments")
-@SoftDelete(columnName = "is_deleted")
 @Data
 public class Comment {
     @Id
@@ -35,7 +34,9 @@ public class Comment {
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
+    @Column(nullable = false)
     private String text;
 
+    @Column(nullable = false)
     private LocalDateTime timeStamp;
 }
