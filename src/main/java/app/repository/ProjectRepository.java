61d7c341641faa76e1e0c9127c11c5b1cdad5da2
makaroshyna <katya23225@gmail.com>
2024-06-01 18:50:34 +0300

    Add repositories.

---
+++ src/main/java/app/repository/ProjectRepository.java
@@ -0,0 +1,9 @@
+package app.repository;
+
+import app.model.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+}
