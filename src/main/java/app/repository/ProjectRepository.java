c066c6ca477e0d3445a324e236ffa3186ee79d83
Valentyn Sharshon <woosay333@gmail.com>
2024-06-02 22:36:25 +0300

    Created ProjectMapper interface

---
+++ src/main/java/app/repository/ProjectRepository.java
@@ -0,0 +1,23 @@
+package app.repository;
+
+import app.model.Project;
+import app.model.ProjectStatus;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
+import java.util.Optional;
+
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+    @Query("FROM Project p JOIN FETCH p.tasks")
+    List<Project> findAllProjects(Pageable pageable);
+
+    @EntityGraph(attributePaths = {"tasks"})
+    Optional<Project> findProjectById(Long id);
+
+    @Modifying
+    @Query("UPDATE Project p SET p.projectStatus = :projectStatus WHERE p.id = :id")
+    int updateProjectStatusById(Long id, ProjectStatus projectStatus);
+}
