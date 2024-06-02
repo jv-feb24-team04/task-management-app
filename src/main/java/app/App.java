34c07b5b8cded2cebd65a4c2a74d532f583eced3
Valentyn Sharshon <woosay333@gmail.com>
2024-06-02 22:35:12 +0300

    Added OpenAPIDefinition annotation

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -1,11 +1,20 @@
 package app;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @RequiredArgsConstructor
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Task management application",
+                version = "0.1",
+                description = "APIs for managing task manager"
+        )
+)
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
