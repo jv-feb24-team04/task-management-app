dfec706bde9fedd3879c26e76eaf77fff7b866eb
makaroshyna <katya23225@gmail.com>
2024-06-02 15:18:24 +0300

    Add messages for validation fields.

--- src/main/java/app/dto/task/CreateTaskRequestDto.java
+++ src/main/java/app/dto/task/CreateTaskRequestDto.java
@@ -11,17 +11,17 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 @Data
 public class CreateTaskRequestDto {
-    @NotBlank
+    @NotBlank(message = "Task name must be defined")
     private String name;
     private String description;
-    @NotNull
+    @NotNull(message = "Priority for the task must be defined")
     private Priority priority;
-    @NotNull
+    @NotNull(message = "Task status must be defined")
     private TaskStatus status;
-    @NotNull
+    @NotNull(message = "Task due date must be defined")
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     @JsonFormat(pattern = "YYYY-MM-dd")
     private LocalDate dueDate;
-    @NotNull
+    @NotNull(message = "Assignee for the task must be defined")
     private Long assigneeId;
 }
