8b3e6c311e7fa0b27ec586ab4c14d5c806488f73
bodya4243 <105318502+bodya4243@users.noreply.github.com>
2024-06-02 13:39:24 +0300

    1) added UserService; controller; repository; 2) added security

--- src/main/java/app/repository/UserRepository.java
+++ src/main/java/app/repository/UserRepository.java
@@ -1,9 +1,9 @@
 package app.repository;
 
-import app.model.User;
 import java.util.Optional;
+import app.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findUserByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
