456f66da98a909d06b976cf69ae0fe278c34f54b
Valentyn Sharshon <woosay333@gmail.com>
2024-06-02 22:37:23 +0300

    Created ProjectServiceImpl class

---
+++ src/main/java/app/service/ProjectServiceImpl.java
@@ -0,0 +1,63 @@
+package app.service;
+
+import app.dto.ProjectRequestDto;
+import app.dto.ProjectResponseDto;
+import app.dto.ProjectStatusDto;
+import app.exception.EntityNotFoundException;
+import app.mapper.ProjectMapper;
+import app.model.Project;
+import app.repository.ProjectRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.BeanUtils;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProjectServiceImpl implements ProjectService {
+    private static final String PROJECT_NOT_FOUND_ERROR_MESSAGE = "Project with id %s not found";
+
+    private final ProjectMapper projectMapper;
+    private final ProjectRepository projectRepository;
+
+    @Override
+    @Transactional
+    public ProjectResponseDto saveProject(ProjectRequestDto projectDto) {
+        Project project = projectMapper.toModel(projectDto);
+        return projectMapper.toDto(projectRepository.save(project));
+    }
+
+    @Override
+    public List<ProjectResponseDto> getAllProjects(Pageable pageable) {
+        return projectRepository.findAllProjects(pageable).stream()
+                .map(projectMapper::toDto)
+                .toList();
+    }
+
+    @Override
+    public ProjectResponseDto getProjectDetailsById(Long projectId) {
+        return projectMapper.toDto(projectRepository.findProjectById(projectId)
+                .orElseThrow(() -> new EntityNotFoundException(
+                        String.format(PROJECT_NOT_FOUND_ERROR_MESSAGE, projectId))));
+    }
+
+    @Override
+    @Transactional
+    public ProjectResponseDto updateProject(Long projectId, ProjectRequestDto projectDto) {
+        Project projectFromDb = projectRepository.findProjectById(projectId)
+                .orElseThrow(() -> new EntityNotFoundException(
+                        String.format(PROJECT_NOT_FOUND_ERROR_MESSAGE, projectId)));
+        BeanUtils.copyProperties(projectDto, projectFromDb);
+        return projectMapper.toDto(projectRepository.save(projectFromDb));
+    }
+
+    @Override
+    @Transactional
+    public void updateProjectStatus(Long id, ProjectStatusDto statusDto) {
+        if (projectRepository.updateProjectStatusById(id, statusDto.projectStatus()) < 1) {
+            throw new EntityNotFoundException(String.format(PROJECT_NOT_FOUND_ERROR_MESSAGE, id));
+        }
+    }
+}
