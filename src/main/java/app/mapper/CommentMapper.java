e677a01fe55edcef55de4a95f2cdbc8b0182427a
Eduard Fakhrutdinov <119575377+EduarfF@users.noreply.github.com>
2024-06-02 19:30:40 +0300

    Add comment controller (#15)
    * added comment entity
    
    * fixed comment entity
    
    * fixed 2 comment entity
    
    * added comment controller
    
    * fixed comment controller
    
    * fixed 2 comment controller
    
    * fixed 3 comment controller
    
    * fixed 4 comment controller

---
+++ src/main/java/app/mapper/CommentMapper.java
@@ -0,0 +1,22 @@
+package app.mapper;
+
+import app.config.MapperConfig;
+import app.dto.comment.CommentRequestDto;
+import app.dto.comment.CommentResponseDto;
+import app.model.Comment;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(config = MapperConfig.class)
+public interface CommentMapper {
+    @Mapping(target = "taskId", source = "task.id")
+    @Mapping(target = "userId", source = "user.id")
+    CommentResponseDto toDto(Comment comment);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "task", ignore = true)
+    @Mapping(target = "user", ignore = true)
+    @Mapping(target = "timeStamp", ignore = true)
+    @Mapping(target = "lastEdit", ignore = true)
+    Comment toEntity(CommentRequestDto requestDto);
+}
---
+++ src/main/java/app/service/comment/CommentService.java
@@ -0,0 +1,18 @@
+package app.service.comment;
+
+import app.dto.comment.CommentRequestDto;
+import app.dto.comment.CommentResponseDto;
+import java.util.List;
+import org.springframework.data.domain.Pageable;
+
+public interface CommentService {
+    CommentResponseDto save(CommentRequestDto commentRequestDto, Long userId, Long taskId);
+
+    List<CommentResponseDto> getByUserId(Long userId, Pageable pageable);
+
+    List<CommentResponseDto> getByTaskId(Long taskId, Pageable pageable);
+
+    CommentResponseDto update(Long commentId, CommentRequestDto commentRequestDto);
+
+    void delete(Long commentId);
+}
