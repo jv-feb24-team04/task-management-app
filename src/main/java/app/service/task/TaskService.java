68e784dc2891ea2f8bf990143f256a8bcd312ad9
Katia Makarchuk <83081198+makaroshyna@users.noreply.github.com>
2024-06-03 11:23:11 +0300

    Change path and add pageable. (#23)

--- src/main/java/app/service/task/TaskService.java
+++ src/main/java/app/service/task/TaskService.java
@@ -4,11 +4,12 @@ import app.dto.task.CreateTaskRequestDto;
 import app.dto.task.TaskDtoWithoutLabelsAndComments;
 import app.dto.task.TaskResponseDto;
 import java.util.List;
+import org.springframework.data.domain.Pageable;
 
 public interface TaskService {
     TaskDtoWithoutLabelsAndComments save(CreateTaskRequestDto requestDto, Long projectId);
 
-    List<TaskResponseDto> getAllByProjectId(Long projectId);
+    List<TaskResponseDto> getAllByProjectId(Long projectId, Pageable pageable);
 
     TaskResponseDto getById(Long projectId, Long id);
 
