57b32da2d7ca46ce6c6708125ff7b8693a723e76
makaroshyna <katya23225@gmail.com>
2024-06-04 16:21:26 +0300

    Add test for getAllForProject() method in LabelService.

--- src/test/java/app/service/label/LabelServiceImplTest.java
+++ src/test/java/app/service/label/LabelServiceImplTest.java
@@ -9,6 +9,7 @@ import app.dto.label.LabelResponseDto;
 import app.mapper.LabelMapper;
 import app.model.Label;
 import app.repository.LabelRepository;
+import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -47,5 +48,24 @@ class LabelServiceImplTest {
         LabelResponseDto result = labelService.create(requestDto);
         assertNotNull(result);
         assertEquals(responseDto.getName(), result.getName());
+    }
+
+    @Test
+    public void getAllForProject_ValidProjectId_ReturnsSetOfLabelResponseDto() {
+        Label label = new Label();
+        label.setName("Label Name");
+        Set<Label> labels = Set.of(label);
+
+        LabelResponseDto responseDto = new LabelResponseDto();
+        responseDto.setName("Label Name");
+        Set<LabelResponseDto> responseDtos = Set.of(responseDto);
+
+        when(labelRepository.findAllByProjectId(1L))
+                .thenReturn(labels);
+        when(labelMapper.toDtoSet(labels)).thenReturn(responseDtos);
+
+        Set<LabelResponseDto> result = labelService.getAllForProject(1L);
+        assertNotNull(result);
+        assertEquals(result.size(), 1);
     }
 }
